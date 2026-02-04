fe7e900b015e6354a82787a1e223d6a9c4c95c10
Ahinsak <ahinsak215@gmail.com>
2026-02-04 12:11:47 +0530

    Build Task Manager app using vanilla JavaScript

--- script.js
+++ script.js
@@ -45,18 +45,58 @@ function toggleTodo(id) {
 }
 
 function deleteTodo(id) {
-  todos = todos.filter((todo) => todo.id !== id);
-  saveAndRender();
+  const li = document.querySelector(`li[data-id="${id}"]`);
+  if (!li) return;
+
+  li.classList.add("deleting");
+
+  setTimeout(() => {
+    li.classList.add("fade-out");
+  }, 50);
+
+  setTimeout(() => {
+    todos = todos.filter((todo) => todo.id !== id);
+    saveAndRender();
+  }, 200);
 }
 
-function editTodo(id) {
-  const todo = todos.find((t) => t.id === id);
-  const updatedText = prompt("Edit task", todo.text);
+function startEdit(li, todo) {
+  if (document.querySelector("li.editing")) return;
 
-  if (updatedText && updatedText.trim()) {
-    todo.text = updatedText.trim();
-    saveAndRender();
+  li.classList.add("editing");
+
+  const span = li.querySelector("span");
+  const originalText = todo.text;
+
+  const editInput = document.createElement("input");
+  editInput.type = "text";
+  editInput.value = originalText;
+  editInput.className = "edit-input";
+
+  li.replaceChild(editInput, span);
+  editInput.focus();
+
+  function save() {
+    const newText = editInput.value.trim();
+    if (newText) {
+      todo.text = newText;
+      saveAndRender();
+    } else {
+      cancel();
+    }
   }
+
+  function cancel() {
+    li.classList.remove("editing");
+    li.replaceChild(span, editInput);
+  }
+
+  editInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") save();
+    if (e.key === "Escape") cancel();
+  });
+
+  editInput.addEventListener("blur", save);
 }
 
 function getFilteredTodos() {
@@ -73,6 +113,8 @@ function renderTodos() {
 
   filtered.forEach((todo) => {
     const li = document.createElement("li");
+    li.dataset.id = todo.id;
+
     if (todo.completed) li.classList.add("completed");
 
     const span = document.createElement("span");
@@ -84,7 +126,7 @@ function renderTodos() {
 
     const editBtn = document.createElement("button");
     editBtn.textContent = "Edit";
-    editBtn.onclick = () => editTodo(todo.id);
+    editBtn.onclick = () => startEdit(li, todo);
 
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "Delete";
